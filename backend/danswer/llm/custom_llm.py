e35217784b58299bdbec865b054dc4f12b667600
Vivek <45358096+vivekjhauipath@users.noreply.github.com>
2024-12-19 15:41:52 +0530

    Handle max tokens

--- backend/danswer/llm/custom_llm.py
+++ backend/danswer/llm/custom_llm.py
@@ -75,7 +75,7 @@ class CustomModelServer(LLM):
         identity_url: str | None = GEN_AI_IDENTITY_ENDPOINT,
         client_id: str | None = GEN_AI_CLIENT_ID,
         client_secret: str | None = GEN_AI_CLIENT_SECRET,
-        max_output_tokens: int = GEN_AI_MAX_OUTPUT_TOKENS,
+        max_output_tokens: int = int(GEN_AI_MAX_OUTPUT_TOKENS),
         api_version: str | None = GEN_AI_API_VERSION,
     ):
 
@@ -123,6 +123,9 @@ class CustomModelServer(LLM):
         self._temperature = 0.0
         self._api_key = api_key
 
+        if max_output_tokens <= 0:
+            self._max_output_tokens = 7000
+
     def _execute(self, input: LanguageModelInput) -> AIMessage:
 
         # print(f"Access Token: {self.token}")
@@ -150,8 +153,9 @@ class CustomModelServer(LLM):
             json_array.append(json_obj)
 
         #print(f"Json Array: {json_array}")
-
+        
         data = {
+            "max_tokens": self._max_output_tokens,
             "messages": json_array
         }
 
