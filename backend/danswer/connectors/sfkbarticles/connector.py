26767688a63e0740279a4bc8e805033d71aa7369
swati354 <swatitiwari354@gmail.com>
2025-03-05 18:35:39 +0530

    Update sf-articles utils

--- backend/danswer/connectors/sfkbarticles/connector.py
+++ backend/danswer/connectors/sfkbarticles/connector.py
@@ -15,7 +15,7 @@ from danswer.connectors.interfaces import SecondsSinceUnixEpoch
 from danswer.connectors.models import BasicExpertInfo
 from danswer.connectors.models import Document
 from danswer.connectors.models import Section
-from danswer.connectors.salesforce.utils import extract_dict_text
+from danswer.connectors.sfkbarticles.utils import extract_dict_text
 from danswer.utils.logger import setup_logger
 
 ID_PREFIX = "SALESFORCE_"
