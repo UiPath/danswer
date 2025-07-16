a013d0fd1fc9c26dca73e9a6466c785139574faa
divijsingh04 <divij.singh@uipath.com>
2025-07-16 14:32:09 +0530

    fix: shift constants to app configs

--- backend/danswer/danswerbot/slack/handlers/handle_buttons.py
+++ backend/danswer/danswerbot/slack/handlers/handle_buttons.py
@@ -9,14 +9,14 @@ from slack_sdk.socket_mode.request import SocketModeRequest
 from sqlalchemy.orm import Session
 
 from danswer.configs.app_configs import CURATED_RESPONSE_CONFIG_KEY
+from danswer.configs.app_configs import DEFAULT_CURATED_RESPONSE_MESSAGE
 from danswer.configs.app_configs import ENABLE_CURATED_RESPONSE_KEY
 from danswer.configs.app_configs import RESPONSE_MESSAGE_KEY
-from danswer.configs.app_configs import USER_TITLE_FILTER_KEY
-from danswer.configs.app_configs import USER_KEY
 from danswer.configs.app_configs import USER_ID_KEY
+from danswer.configs.app_configs import USER_KEY
 from danswer.configs.app_configs import USER_PROFILE_KEY
+from danswer.configs.app_configs import USER_TITLE_FILTER_KEY
 from danswer.configs.app_configs import USER_TITLE_KEY
-from danswer.configs.app_configs import DEFAULT_CURATED_RESPONSE_MESSAGE
 from danswer.configs.constants import SearchFeedbackType
 from danswer.configs.danswerbot_configs import DANSWER_FOLLOWUP_EMOJI
 from danswer.connectors.slack.utils import make_slack_api_rate_limited
@@ -47,6 +47,7 @@ from danswer.document_index.factory import get_default_document_index
 from danswer.utils.logger import setup_logger
 
 logger_base = setup_logger()
+
 
 def handle_curated_response(
     slack_bot_config: Any,
